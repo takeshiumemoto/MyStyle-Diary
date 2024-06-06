3ec2153e02f9d57748d29c5540f801b4af1f9739
EC2 Default User <ec2-user@ip-172-31-26-95.ap-northeast-1.compute.internal>
2024-06-06 15:16:04 +0000

    view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,56 +1,59 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :is_matching_login_user,except:[:index]
-    def new
-        @post = Post.new
-    end    
-    def create
-        @post = Post.new(post_params)
-        @post.user_id = current_user.id
-     if @post.save
-      redirect_to posts_path, notice: '投稿が作成されました。'
-     else
+  before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path, notice: '投稿が成功しました'
+    else
       render :new
-     end
     end
-    
-    def index
-        @post = Post.new
-        @posts = Post.order(created_at: :desc).limit(8)
-        @user = current_user
-    end 
-    
-    def show
-        @post = Post.new
-        @post = Post.find(params[:id])
-    end     
-    
-    def edit
-        @post = Post.find(params[:id])
-    end 
-    def update
+  end
+
+  def index
+    @posts = Post.all.order(created_at: :desc).limit(8)
+    @user = current_user
+  end
+
+  def show
     @post = Post.find(params[:id])
-     if @post.update(post_params)
-      redirect_to post_path(@post), notice: '投稿が更新されました。'
-     else
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: '投稿が更新されました'
+    else
       render :edit
-     end
     end
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy
-        redirect_to posts_path, notice: '投稿が削除されました。'
-    end     
-    
-    private
-    def post_params
-        params.require(:post).permit(:title,:content,:image)
-    end     
-    
-    def is_matching_login_user
-        @post = Post.find(params[:id])
-        unless @post.user == current_user
-         redirect_to posts_path, alert: 'ログインが必要です'
-        end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: '投稿が削除されました'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :title, :content)
+  end
+
+  def is_matching_login_user
+    @post = Post.find(params[:id])
+    unless @post.user == current_user
+      redirect_to posts_path, alert: 'ログインが必要です'
     end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,27 @@ class UsersController < ApplicationController
        @user = User.find(params[:id])
     end     
     
-    private 
-        def is_matching_login_user
-            unless user_signed_in?&&current_user.id==params[:id].to_i
-                flash[:alert]="ログインしてください"
-                redirect_to root_path
-            end 
-        end     
+    def edit 
+        @user = User.find(params[:id])
+    end 
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to user_path(@user), notice: 'プロフィールが更新されました。'
+        else
+            render :edit, alert: 'プロフィールの更新に失敗しました。'
+        end
+    end
+private 
+    def is_matching_login_user
+        unless user_signed_in?&&current_user.id==params[:id].to_i
+            flash[:alert]="ログインしてください"
+            redirect_to root_path
+        end 
+    end     
+        
+    def user_params
+        params.require(:user).permit(:name,:profile_image)
+    end     
 end
