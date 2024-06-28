c19a6d6335df5e905ed91b844cf01d66a8a589bf
EC2 Default User <ec2-user@ip-172-31-26-95.ap-northeast-1.compute.internal>
2024-06-28 14:55:42 +0000

    change js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,25 @@ import Turbolinks from 'turbolinks';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import $ from 'jquery';
-import 'packs/calendar'; // Assuming calendar.js is in packs
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Popper from 'popper.js'; // 正しいPopper.jsをインポート
+import Popper from 'popper.js';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// グローバルに設定
 window.$ = $;
 window.jQuery = $;
-window.Popper = Popper; // Popperをグローバルに設定
+window.Popper = Popper;
 
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
 console.log("Document ready. Initializing flatpickr...");
 $(document).on('turbolinks:load', function() {
@@ -26,11 +35,61 @@ $(document).on('turbolinks:load', function() {
   } catch (error) {
     console.error("Error initializing flatpickr:", error);
   }
+
+  function clearCalendar() {
+    $('#calendar').html('');
+  }
+
+  const calendarEl = document.getElementById('calendar');
+  if (calendarEl) {
+    const calendar = new Calendar(calendarEl, {
+      plugins: [dayGridPlugin, listPlugin, interactionPlugin],
+      initialView: 'dayGridMonth',
+      locale: 'ja',
+      events: {
+        url: '/events.json',
+        method: 'GET',
+        failure: function() {
+          alert('イベントの取得中にエラーが発生しました。');
+        }
+      },
+      windowResize: function () {
+        if (window.innerWidth < 992) {
+          calendar.changeView('listMonth');
+        } else {
+          calendar.changeView('dayGridMonth');
+        }
+      },
+      dateClick: function(info) {
+        const date = info.date;
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        const formattedDate = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}T00:00`;  // クリックした日付をフォーマット
+
+        $.ajax({
+          type: 'GET',
+          url: '/events/new',
+          headers: {
+            'Accept': 'text/html'
+          },
+          success: function(res) {
+            $('.modal-body').html(res);
+            flatpickr('.datetime-picker', {
+              enableTime: true,
+              dateFormat: 'Y-m-dTH:i',
+              defaultDate: formattedDate  // フォームの初期日付にセット
+            });
+            $('#modal').modal('show');
+          },
+          error: function() {
+            alert('エラーが発生しました。運営に問い合わせてください。');
+          }
+        });
+      }
+    });
+    calendar.render();
+  }
+
+  $(document).on('turbolinks:before-cache', clearCalendar);
 });
-
-
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
-
-
--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -3,64 +3,64 @@ import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import listPlugin from '@fullcalendar/list';
 import interactionPlugin from '@fullcalendar/interaction';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import flatpickr from 'flatpickr';  // flatpickrをインポート
 import 'flatpickr/dist/flatpickr.min.css';
+
 $(document).on('turbolinks:load', function () {
-    function clearCalendar() {
-        $('#calendar').html('');
-    }
+  function clearCalendar() {
+    $('#calendar').html('');
+  }
 
-    const calendarEl = document.getElementById('calendar');
-    if (calendarEl) {
-        const calendar = new Calendar(calendarEl, {
-            plugins: [dayGridPlugin, listPlugin, interactionPlugin],
-            initialView: 'dayGridMonth',
-            locale: 'ja',
-            events: {
-                url: '/events.json',
-                method: 'GET',
-                failure: function() {
-                    alert('イベントの取得中にエラーが発生しました。');
-                }
-            },
-            windowResize: function () {
-                if (window.innerWidth < 992) {
-                    calendar.changeView('listMonth');
-                } else {
-                    calendar.changeView('dayGridMonth');
-                }
-            },
-            dateClick: function(info) {
-                const date = info.date;
-                const year = date.getFullYear();
-                const month = date.getMonth() + 1;
-                const day = date.getDate();
-                const formattedDate = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}T00:00`;  // クリックした日付をフォーマット
+  const calendarEl = document.getElementById('calendar');
+  if (calendarEl) {
+    const calendar = new Calendar(calendarEl, {
+      plugins: [dayGridPlugin, listPlugin, interactionPlugin],
+      initialView: 'dayGridMonth',
+      locale: 'ja',
+      events: {
+        url: '/events.json',
+        method: 'GET',
+        failure: function() {
+          alert('イベントの取得中にエラーが発生しました。');
+        }
+      },
+      windowResize: function () {
+        if (window.innerWidth < 992) {
+          calendar.changeView('listMonth');
+        } else {
+          calendar.changeView('dayGridMonth');
+        }
+      },
+      dateClick: function(info) {
+        const date = info.date;
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        const formattedDate = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}T00:00`;  // クリックした日付をフォーマット
 
-                $.ajax({
-                    type: 'GET',
-                    url: '/events/new',
-                    headers: {
-                        'Accept': 'text/html'
-                    },
-                    success: function(res) {
-                        $('.modal-body').html(res);
-                        flatpickr('.datetime-picker', {
-                            enableTime: true,
-                            dateFormat: 'Y-m-dTH:i',
-                            defaultDate: formattedDate  // フォームの初期日付にセット
-                        });
-                        $('#modal').modal('show');
-                    },
-                    error: function() {
-                        alert('エラーが発生しました。運営に問い合わせてください。');
-                    }
-                });
-            }
+        $.ajax({
+          type: 'GET',
+          url: '/events/new',
+          headers: {
+            'Accept': 'text/html'
+          },
+          success: function(res) {
+            $('.modal-body').html(res);
+            flatpickr('.datetime-picker', {
+              enableTime: true,
+              dateFormat: 'Y-m-dTH:i',
+              defaultDate: formattedDate  // フォームの初期日付にセット
+            });
+            $('#modal').modal('show');
+          },
+          error: function() {
+            alert('エラーが発生しました。運営に問い合わせてください。');
+          }
         });
-        calendar.render();
-    }
+      }
+    });
+    calendar.render();
+  }
 
-    $(document).on('turbolinks:before-cache', clearCalendar);
+  $(document).on('turbolinks:before-cache', clearCalendar);
 });
