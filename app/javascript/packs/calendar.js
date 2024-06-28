0d9db3054b49ef5515e22517921a799d1e6592ec
EC2 Default User <ec2-user@ip-172-31-26-95.ap-northeast-1.compute.internal>
2024-06-28 16:01:35 +0000

    aaa

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -8,60 +8,60 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 $(document).on('turbolinks:load', function () {
-  function clearCalendar() {
-    $('#calendar').html('');
-  }
+    function clearCalendar() {
+        $('#calendar').html('');
+    }
 
-  const calendarEl = document.getElementById('calendar');
-  if (calendarEl) {
-    const calendar = new Calendar(calendarEl, {
-      plugins: [dayGridPlugin, listPlugin, interactionPlugin],
-      initialView: 'dayGridMonth',
-      locale: 'ja',
-      events: {
-        url: '/events.json',
-        method: 'GET',
-        failure: function() {
-          alert('イベントの取得中にエラーが発生しました。');
-        }
-      },
-      windowResize: function () {
-        if (window.innerWidth < 992) {
-          calendar.changeView('listMonth');
-        } else {
-          calendar.changeView('dayGridMonth');
-        }
-      },
-      dateClick: function(info) {
-        const date = info.date;
-        const year = date.getFullYear();
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
-        const formattedDate = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}T00:00`;
+    const calendarEl = document.getElementById('calendar');
+    if (calendarEl) {
+        const calendar = new Calendar(calendarEl, {
+            plugins: [dayGridPlugin, listPlugin, interactionPlugin],
+            initialView: 'dayGridMonth',
+            locale: 'ja',
+            events: {
+                url: '/events.json',
+                method: 'GET',
+                failure: function() {
+                    alert('イベントの取得中にエラーが発生しました。');
+                }
+            },
+            windowResize: function () {
+                if (window.innerWidth < 992) {
+                    calendar.changeView('listMonth');
+                } else {
+                    calendar.changeView('dayGridMonth');
+                }
+            },
+            dateClick: function(info) {
+                const date = info.date;
+                const year = date.getFullYear();
+                const month = date.getMonth() + 1;
+                const day = date.getDate();
+                const formattedDate = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}T00:00`;  // クリックした日付をフォーマット
 
-        $.ajax({
-          type: 'GET',
-          url: '/events/new',
-          headers: {
-            'Accept': 'text/html'
-          },
-          success: function(res) {
-            $('.modal-body').html(res);
-            flatpickr('.datetime-picker', {
-              enableTime: true,
-              dateFormat: 'Y-m-dTH:i',
-              defaultDate: formattedDate
-            });
-            $('#modal').modal('show');
-          },
-          error: function() {
-            alert('エラーが発生しました。運営に問い合わせてください。');
-          }
+                $.ajax({
+                    type: 'GET',
+                    url: '/events/new',
+                    headers: {
+                        'Accept': 'text/html'
+                    },
+                    success: function(res) {
+                        $('.modal-body').html(res);
+                        flatpickr('.datetime-picker', {
+                            enableTime: true,
+                            dateFormat: 'Y-m-dTH:i',
+                            defaultDate: formattedDate  // フォームの初期日付にセット
+                        });
+                        $('#modal').modal('show');
+                    },
+                    error: function() {
+                        alert('エラーが発生しました。運営に問い合わせてください。');
+                    }
+                });
+            }
         });
-      }
-    });
-    calendar.render();
-  }
+        calendar.render();
+    }
 
-  $(document).on('turbolinks:before-cache', clearCalendar);
+    $(document).on('turbolinks:before-cache', clearCalendar);
 });
